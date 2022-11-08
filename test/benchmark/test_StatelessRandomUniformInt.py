6e340386cb9c0a4a5fe2c97fd34afc7401890743
SherryIn <113427278+SherryIn@users.noreply.github.com>
2022-11-08 22:46:54 +0800

    update ut for RandomUniformInt because of shape (#1541)

--- test/benchmark/test_StatelessRandomUniformInt.py
+++ test/benchmark/test_StatelessRandomUniformInt.py
@@ -34,7 +34,7 @@ ITERATION = 5
 
 class StatelessRandomUniformIntTest(test.TestCase):
     def _test_impl(self, size, dtype):
-        seed1 = 79
+        seed1 = 79, 25
         minval2 = constant_op.constant(1, dtype=dtype)
         maxval2 = constant_op.constant(50, dtype=dtype)
         flush_cache()
