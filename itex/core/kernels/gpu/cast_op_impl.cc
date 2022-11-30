0ba0a049d19c7eca145988f7ed2c65bb8367feec
Li, Guizi <guizi.li@intel.com>
2022-11-30 13:19:24 +0800

    [OP] enable int64 to bfloat16 cast (#1635)

--- itex/core/kernels/gpu/cast_op_impl.cc
+++ itex/core/kernels/gpu/cast_op_impl.cc
@@ -47,7 +47,7 @@ CastFunctorType GetGpuCastFromInt32(DataType dst_dtype) {
 }
 
 CastFunctorType GetGpuCastFromInt64(DataType dst_dtype) {
-  CURRY_TYPES3_NO_BF16(CAST_CASE, GPUDevice, int64);
+  CURRY_TYPES3(CAST_CASE, GPUDevice, int64);
   return nullptr;
 }
 
