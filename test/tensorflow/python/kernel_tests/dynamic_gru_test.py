97125a98e8784f2cc8fcb30c4ec39fc67186648a
Lu Teng <teng.lu@intel.com>
2023-01-04 13:02:45 +0800

    [UT] Fix GPU GRU UT (#1745)

--- test/tensorflow/python/kernel_tests/dynamic_gru_test.py
+++ test/tensorflow/python/kernel_tests/dynamic_gru_test.py
@@ -1,31 +1,48 @@
+
+from intel_extension_for_tensorflow.python.test_func import test_util
+from intel_extension_for_tensorflow.python.test_func import test
+
+from tensorflow.python.client import session
+from tensorflow.python.framework import dtypes
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import rnn
+from tensorflow.python.ops import rnn_cell_impl
+from tensorflow.python.ops import variables
+
 import numpy as np
-import tensorflow as tf
 
-time_steps = 8
-num_units = 3
-input_size = 5
-batch_size = 2
+class DynamicGRUOpTest(test.TestCase):
 
-input_values = np.random.randn(time_steps, batch_size, input_size)
-sequence_length = np.random.randint(0, time_steps, size=batch_size)
+  @test_util.deprecated_graph_mode_only
+  def testDynamicGRU(self):
+    time_steps = 8
+    num_units = 5
+    input_size = 5
+    batch_size = 2
 
-with tf.compat.v1.Session() as sess:
-  concat_inputs =  tf.compat.v1.placeholder(
-      tf.dtypes.float32, shape=(time_steps, batch_size, input_size))
+    input_values = np.random.randn(time_steps, batch_size, input_size)
+    sequence_length = np.random.randint(0, time_steps, size=batch_size)
 
-  cell = tf.compat.v1.nn.rnn_cell.GRUCell(num_units=num_units)
+    concat_inputs =  array_ops.placeholder(
+        dtypes.float32, shape=(time_steps, batch_size, input_size))
 
-  with tf.compat.v1.variable_scope("dynamic_scope"):
-    outputs_dynamic, state_dynamic = tf.compat.v1.nn.dynamic_rnn(
+    cell = rnn_cell_impl.GRUCell(num_units=num_units)
+
+    outputs_dynamic, state_dynamic = rnn.dynamic_rnn(
         cell,
         inputs=concat_inputs,
         sequence_length=sequence_length,
         time_major=True,
-        dtype=tf.dtypes.float32)
+        dtype=dtypes.float32)
 
-  feeds = {concat_inputs: input_values}
+    feeds = {concat_inputs: input_values}
 
-  # Initialize
-  tf.compat.v1.global_variables_initializer().run(feed_dict=feeds)
+    with self.session() as sess:
+      variables.global_variables_initializer().run(feed_dict=feeds)
+      sess.run([outputs_dynamic, state_dynamic], feed_dict=feeds)
 
-  sess.run([outputs_dynamic, state_dynamic], feed_dict=feeds)
+    self.assertEqual(outputs_dynamic.shape, concat_inputs.shape)
+
+
+if __name__ == "__main__":
+  test.main()
