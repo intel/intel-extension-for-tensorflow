7964b6739e0b7e72fa0d6ed11fadc2fe7625f5b6
Sheng Yang <yang.sheng@intel.com>
2023-02-16 10:04:32 +0800

    [Bug Fix] Fix set_config API (#1811)

--- test/tensorflow/python/python_api/Python_API_UT/test_set_get_config.py
+++ test/tensorflow/python/python_api/Python_API_UT/test_set_get_config.py
@@ -14,25 +14,72 @@
 # ==============================================================================
 
 
+import numpy as np
 import tensorflow as tf
 import intel_extension_for_tensorflow as itex
 from intel_extension_for_tensorflow.python.test_func import test_util
 from intel_extension_for_tensorflow.python.test_func import test
-from intel_extension_for_tensorflow.core.utils.protobuf import config_pb2
+from tensorflow.core.protobuf import config_pb2
+from tensorflow.python.ops import array_ops
 
+tf.compat.v1.disable_eager_execution()
 class SetGetConfigTest(test_util.TensorFlowTestCase):
     """test set_config and get_config itex python api"""
 
     @test_util.run_deprecated_v1
     def testSetGetConfig_gpu(self):
-        graph_options = config_pb2.GraphOptions()
-        graph_options.auto_mixed_precision = config_pb2.ON
-        cfg = config_pb2.ConfigProto(graph_options=graph_options)
+        graph_options = itex.GraphOptions()
+        graph_options.auto_mixed_precision = itex.ON
+        cfg = itex.ConfigProto(graph_options=graph_options)
 
         itex.set_config(cfg)
         self.assertProtoEquals("""
           graph_options { auto_mixed_precision: ON }
         """, itex.get_config())
+
+    def testSetConfig(self):
+        x = tf.compat.v1.placeholder(tf.float32, shape=(3, 4))
+        y = tf.compat.v1.placeholder(tf.float32, shape=(4, 5))
+        b = np.random.rand(5).astype(np.float32)
+
+        x_arr = np.random.rand(3, 4)
+        y_arr = np.random.rand(4, 5)
+        run_options = config_pb2.RunOptions(output_partition_graphs=True)
+        metadata = config_pb2.RunMetadata()
+        fused = tf.nn.bias_add(tf.matmul(x, y), b)
+        fused = array_ops.identity(fused)
+
+        ## Disable remapper
+        graph_options = itex.GraphOptions(remapper=itex.OFF)
+        cfg = itex.ConfigProto(graph_options=graph_options)
+        itex.set_config(cfg)
+
+        with self.session(use_gpu=True) as sess:
+            sess.run(fused, feed_dict={x: x_arr, y: y_arr}, options=run_options, run_metadata=metadata)
+            graph = metadata.partition_graphs[0]
+            found_fused_op = False
+            for node in graph.node:
+                if node.op in ('_ITEXFusedMatMul'):
+                    fused_ops = node.attr['fused_ops'].list.s
+                    found_fused_op = len(fused_ops) == 1 and fused_ops[0] == b'BiasAdd'
+                    break
+            self.assertFalse(found_fused_op, "itex.set_config does not work!")
+
+        ## Enable remapper
+        graph_options = itex.GraphOptions(remapper=itex.ON)
+        cfg = itex.ConfigProto(graph_options=graph_options)
+        itex.set_config(cfg)
+
+        with self.session(use_gpu=True) as sess:
+            sess.run(fused, feed_dict={x: x_arr, y: y_arr}, options=run_options, run_metadata=metadata)
+            graph = metadata.partition_graphs[0]
+            found_fused_op = False
+            for node in graph.node:
+                if node.op in ('_ITEXFusedMatMul'):
+                    fused_ops = node.attr['fused_ops'].list.s
+                    found_fused_op = len(fused_ops) == 1 and fused_ops[0] == b'BiasAdd'
+                    break
+            self.assertTrue(found_fused_op, "itex.set_config does not work!")
 
 if __name__ == "__main__":
     test.main()
