210b1d54b153997ebe51d2c0bffbcad9cc398b83
Zhefeng, Qiao <zhefeng.qiao@intel.com>
2023-10-27 15:27:18 +0800

    Update ITEX version to v2.14.0.1 (#2468)

--- itex/python/version.py
+++ itex/python/version.py
@@ -16,4 +16,4 @@
 # ==============================================================================
 '''version information for Intel ® Extension for TensorFlow*'''
 
-__version__ = '2.14.0.0'
+__version__ = '2.14.0.1'
