212c2eaf95f9ff57a7fbdf8f49bae39f4ab70963
Li, Guizi <guizi.li@intel.com>
2023-11-17 13:38:40 +0800

    [Build] update oneapi location (#2500)

--- configure.py
+++ configure.py
@@ -33,7 +33,7 @@ except ImportError:
 # pylint: enable=g-import-not-at-top
 
 
-_DEFAULT_DPCPP_TOOLKIT_PATH = '/opt/intel/oneapi/compiler/latest/linux'
+_DEFAULT_DPCPP_TOOLKIT_PATH = '/opt/intel/oneapi/compiler/latest'
 _DEFAULT_MKL_PATH='/opt/intel/oneapi/mkl/latest'
 _DEFAULT_AOT_CONFIG = ''
 _DEFAULT_GCC_TOOLCHAIN_PATH = ''
