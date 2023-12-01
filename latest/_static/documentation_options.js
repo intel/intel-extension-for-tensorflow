34d55a77df74b387d5e012c3b16c8f8a4034b75c
guizili0 <guizili0@users.noreply.github.com>
2023-12-01 06:46:14 +0000

    deploy: 3e3adb262d01c487aa02ff558515e08a5f51d238

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g60ea3f1',
+    VERSION: '0.1.dev1+g3e3adb2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
