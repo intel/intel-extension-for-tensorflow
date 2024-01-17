76d12aedcdfb3cf1f144811256cbc8cb8cb8dc5e
Cui, Yifeng <yifeng.cui@intel.com>
2024-01-17 13:45:23 +0800

    Update version to 2.14.0.2

--- itex/python/version.py
+++ itex/python/version.py
@@ -16,4 +16,4 @@
 # ==============================================================================
 '''version information for Intel ® Extension for TensorFlow*'''
 
-__version__ = '2.14.0.1'
+__version__ = '2.14.0.2'
