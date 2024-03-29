aea416a721c0a982130960e036e7df4c01e0557b
zofia <110436990+zufangzhu@users.noreply.github.com>
2024-03-29 11:01:52 +0800

    [Docker] Add env to make xelink usable (#2662)

--- docker/horovod-vars.sh
+++ docker/horovod-vars.sh
@@ -19,4 +19,5 @@ export LD_LIBRARY_PATH=/opt/intel/oneapi/redist/opt/mpi/libfabric/lib:$LD_LIBRAR
 export PATH=/opt/intel/oneapi/redist/bin:$PATH
 export I_MPI_ROOT=/opt/intel/oneapi/redist/lib
 export CCL_ROOT=/opt/intel/oneapi/redist
+export CCL_TOPO_FABRIC_VERTEX_CONNECTION_CHECK=0
 export FI_PROVIDER_PATH=/opt/intel/oneapi/redist/opt/mpi/libfabric/lib/prov
