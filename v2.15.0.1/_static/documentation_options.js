6a3640a7879111aa5dd7ee495d40674eae4ca0ed
Neo Zhang Jianyu <jianyu.zhang@intel.com>
2024-08-13 10:42:03 +0800

    fix search issue in online doc (#2741)
    Co-authored-by: ZhangJianyu <zhang.jianyu@outlook.com>

--- v2.15.0.1/_static/documentation_options.js
+++ v2.15.0.1/_static/documentation_options.js
@@ -1,5 +1,4 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+const DOCUMENTATION_OPTIONS = {
     VERSION: 'v2.15.0.1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
